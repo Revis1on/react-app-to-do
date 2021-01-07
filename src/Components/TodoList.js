b00fef0560709cff138a3fd4efebea73d192b04f
FilipRisteski <38330826+Revis1on@users.noreply.github.com>
2021-01-07 22:06:54 +0100

    ver 0.3

---
+++ src/Components/TodoList.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import TodoForm from './TodoForm'
+
+function TodoList() {
+
+    const[todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>What's the Plan for Today</h1>
+            <TodoForm />
+        </div>
+    )
+}
+
+export default TodoList
