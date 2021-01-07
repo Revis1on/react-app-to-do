a0ae3b2aea934f2bda031ab8762aeaec0d9f3198
FilipRisteski <38330826+Revis1on@users.noreply.github.com>
2021-01-07 22:34:27 +0100

    ver 0.6

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
 
 function TodoList() {
@@ -9,13 +10,30 @@ function TodoList() {
         if(!todo.text || /^\s*$/.test(todo.text)){
             return
         }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos);
+    };
+    
+
+    const completeTodo = id =>{
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id){
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
     }
 
 
     return (
         <div>
             <h1>What's the Plan for Today</h1>
-            <TodoForm />
+            <TodoForm onSubmit={addTodo} />
+            <Todo
+            todos={todos} 
+            completeTodo={completeTodo}/>
         </div>
     )
 }
